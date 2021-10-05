001a038f45bbec6880e69d8326a106033b8ef4a9
ghramcode18 <81458028+ghramcode18@users.noreply.github.com>
2021-10-05 23:33:10 +0300

    --

--- src/main/java/delicious/delicious/models/RecipeModel.java
+++ src/main/java/delicious/delicious/models/RecipeModel.java
@@ -1,5 +1,6 @@
 package delicious.delicious.models;
 
+import delicious.delicious.entities.RecipeEntity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -19,7 +20,8 @@ public class RecipeModel {
  private String name;
  private  double price;
  private String type;
- private String steps;
+ private String image;
+ private Recipe_StepsModel Recipe_steps;
  private String  imgrate;
     
 }
--- src/main/java/delicious/delicious/services/UserService.java
+++ src/main/java/delicious/delicious/services/UserService.java
@@ -1,15 +1,13 @@
 package delicious.delicious.services;
 
-import java.util.List;
 import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
+import delicious.delicious.entities.RecipeEntity;
 import delicious.delicious.entities.UserEntity;
 import delicious.delicious.exceptions.UserException;
-import delicious.delicious.mappers.RecipeMapper;
-// import delicious.delicious.mappers.UserMapper;
 import delicious.delicious.models.RecipeModel;
 import delicious.delicious.models.UserModel;
 import delicious.delicious.repositories.RecipeRepo;
@@ -22,12 +20,12 @@ public class UserService {
     UserRepo userRepo;
     @Autowired
     RecipeRepo recipeRepo;
-   
+
     public UserModel registerUser(UserModel user) throws UserException {
         if (user.getEmail() != null && user.getPassword() != null && user.getUserName() != null) {
             if (userRepo.findByEmail(user.getEmail()).isEmpty()) {
                 UserEntity entity = userRepo.save(UserModelToUserEntity(user));
-                return user.builder().userId(entity.getId()).password(entity.getPassword()).email(entity.getEmail())
+                return user.builder().id(entity.getId()).password(entity.getPassword()).email(entity.getEmail())
                         .userName(entity.getUserName()).build();
             } else {
                 throw new UserException("this email is already exist");
@@ -39,56 +37,53 @@ public class UserService {
     }
 
     public UserEntity UserModelToUserEntity(UserModel user) {
-        UserEntity entity = new UserEntity().builder().email(user.getEmail()).userName(user.getUserName())
-                .id(user.getUserId()).password(user.getPassword()).build();
+        UserEntity entity = new UserEntity().builder().id(user.getId()).email(user.getEmail())
+                .userName(user.getUserName()).password(user.getPassword()).favorites(user.getUserFav()).build();
 
         return entity;
 
     }
 
-    // TODO please check my @Bean
+    public UserModel signIn(UserModel user) throws UserException {
+        Optional<UserEntity> entity;
+        if ((entity = userRepo.findByEmail(user.getEmail())).isEmpty()) {
+            throw new UserException("no user with this email");
+        } else {
+            if (entity.get().getPassword().equals(user.getPassword())) {
+                return UserEntityToUserModel(entity.get());
+            } else {
+                throw new UserException("wrong password");
+            }
 
-    // public UserModel registerUser(UserModel user) throws UserException {
-    // if (user.getEmail() != null && user.getPassword() != null &&
-    // user.getUserName() != null) {
-    // if (userRepo.findByEmail(user.getEmail()).isEmpty()) {
-    // UserEntity entity = userRepo.save(userMapper.UserModelToUserEntity(user));
-    // return user.builder().userId(entity.getId()).build();
-    // } else {
-    // throw new UserException("this email is already exist");
-    // }
-    // } else {
-    // throw new UserException("required fields are null");
+        }
+    }
 
-    // }
-    // }
+    public UserModel UserEntityToUserModel(UserEntity user) {
+        UserModel entity = new UserModel().builder().id(user.getId()).email(user.getEmail())
+                .userName(user.getUserName()).password(user.getPassword()).UserFav(user.getFavorites()).build();
 
-    // public UserModel signIn(UserModel user) throws UserException {
-    // Optional<UserEntity> entity;
-    // if ((entity = userRepo.findByEmail(user.getEmail())).isEmpty()) {
-    // throw new UserException("no user with this email");
-    // } else {
-    // if (entity.get().getPassword().equals(user.getPassword())) {
-    // return UserMapper.mapper.UserEntityToUserModel(entity.get());
-    // } else {
-    // throw new UserException("wrong password");
-    // }
+        return entity;
 
-    // }
-    // }
+    }
 
-    // public UserModel addFavorite(Integer id, RecipeModel rModel) {
-    // if (id == null)
-    // throw new UserException("can't user or recipe with this id");
+    public UserModel addFavorite(Integer id, RecipeModel rModel) {
+        if (id == null)
+            throw new UserException("can't user or recipe with this id");
 
-    // UserEntity entity = userRepo.findById(id).orElseThrow(() -> new
-    // UserException("no user with this id"));
-    // entity.getFavorites().add(RecipeMapper.RecipeModelToRecipeEntity(rModel));
-    // userRepo.save(entity);
-    // return UserMapper.mapper.UserEntityToUserModel(entity);
+        UserEntity entity = userRepo.findById(id).orElseThrow(() -> new UserException("no user with this id"));
+        
+        entity.getFavorites().add(RecipeModelToRecipeEntity(rModel));
+        userRepo.save(entity);
+        return UserEntityToUserModel(entity);
 
-    // }
+    }
 
+    private RecipeEntity RecipeModelToRecipeEntity(RecipeModel rModel) {
+        RecipeEntity rEntity = new RecipeEntity();
+        rEntity.builder().id(rModel.getId()).name(rModel.getName()).image(rModel.getImage()).type(rModel.getType())
+                .price(rModel.getPrice()).imgrate(rModel.getImgrate()).build();
+        return rEntity;
+    }
     // public List<RecipeModel> getFavorite(Integer id) {
     // if (id == null)
     // throw new UserException("can't user with this id");
