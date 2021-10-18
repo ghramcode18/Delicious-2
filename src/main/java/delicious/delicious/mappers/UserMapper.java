7012ec9e4fab70f34bb3e3c3cc1cc6c69586b9b5
ghramcode18 <81458028+ghramcode18@users.noreply.github.com>
2021-10-18 19:25:09 +0300

    --

---
+++ src/main/java/delicious/delicious/mappers/UserMapper.java
@@ -0,0 +1,26 @@
+package delicious.delicious.mappers;
+
+import java.util.List;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+import delicious.delicious.entities.UserEntity;
+import delicious.delicious.models.UserModel;
+
+
+@Mapper
+public interface UserMapper 
+{//CAN I PUT ALL OF MAPPERIN ONE CLASSE?
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+    @Mapping (source = "",target = "")
+    UserEntity UserModelToUserEntity(UserModel UserModel);
+    UserModel UserEntityToUserModel(UserEntity UserEntity);
+    List<UserEntity> UserModelToUserEntityAslist(List<UserModel> UserModel);
+    List<UserModel> UserEntityToUserModelAsList(List<UserEntity> userEntities);
+
+}
+ 
+// //source the name in Entity and target the name in Model.
+
